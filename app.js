e9f83d64ab43f0838908f7b7a694c4290607bd60
panchaltrilok786coder <panchaltrilok786@gmail.com>
2026-08-24 15:38:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -228,7 +228,7 @@ function subscribe(name, constraints, onData, onError) {
 
 async function login(email, password) {
   const credential = await signInWithEmailAndPassword(auth, email, password);
-alert(credential);
+alert(JSON.stringify(credential));
   const profile = await getById(COLLECTIONS.USERS, credential.user.uid);
   if (!profile) {
     await signOut(auth);
